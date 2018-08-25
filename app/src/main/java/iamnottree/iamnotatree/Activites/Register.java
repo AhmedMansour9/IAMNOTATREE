3b4fefe3ad5ec205f152a56c1eeca74a8b24ad7a
HP <ahmed_pop219490@yahoo.com>
2018-08-25 17:24:34 +0200

    :)

--- app/src/main/java/iamnottree/iamnotatree/Activites/Register.java
+++ app/src/main/java/iamnottree/iamnotatree/Activites/Register.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import iamnottree.iamnotatree.R;
 
@@ -12,11 +14,55 @@ import iamnottree.iamnotatree.R;
  */
 
 public class Register extends Activity {
+    EditText User_Name,Email,Phone,Password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        User_Name=findViewById(R.id.User_Name);
+        Email=findViewById(R.id.Email);
+        Phone=findViewById(R.id.Phone);
+        Password=findViewById(R.id.Password);
 
+    }
+
+    public Boolean ValidUserName(){
+        String User=User_Name.getText().toString().trim();
+        if(User.isEmpty()||User.length()<5){
+           User_Name.setError("ادخل اسم حسابك صحيح ");
+            return false;
+        }else if(!User.matches(("^[a-zA-Z\\s]*$"))){
+            User_Name.setError("ادخل اسم حسابك صحيح ");
+            return false;
+        }
+     return true;
+    }
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private Boolean ValidateEmail(){
+        String EMAIL=Email.getText().toString().trim();
+        if (EMAIL.isEmpty()||!isValidEmail(EMAIL)){
+            Email.setError(("ادخل البريد الالكتروني "));
+
+            return false;
+        }else if(!EMAIL.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
+            Email.setError(("ادخل البريد الالكتروني "));
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean ValidarePassword(){
+        if(Phone.getText().toString().length()!=11){
+            Phone.setError("ادخل رقم الموبايل صحيح");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
